b5af619dba1f33a5cfb63c1cba3784cb591ce285
Ethics <anindo.opondo@strathmore.edu>
2021-12-01 19:21:28 +0300

    tracking

--- app/src/main/java/com/challengers/trackmyorder/LoginActivity.java
+++ app/src/main/java/com/challengers/trackmyorder/LoginActivity.java
@@ -2,23 +2,39 @@ package com.challengers.trackmyorder;
 
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.challengers.trackmyorder.util.Constants;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginActivity extends AppCompatActivity {
+//import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity<password, email> extends AppCompatActivity {
     EditText userIdEditText,userPassEditText;
     String userName,userPass, loginType;
+    EditText mEmail;
+    EditText mPassword;
+    Button mLoginBtn;
+    TextView mCreateBtn;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_dboy);
         setTitle("Login");
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mLoginBtn = findViewById(R.id.loginbtn);
+        mCreateBtn = findViewById(R.id.createText);
+        fAuth = FirebaseAuth.getInstance();
         userIdEditText = (EditText) findViewById(R.id.userName);
         userPassEditText = (EditText) findViewById(R.id.userPass);
 
@@ -28,25 +44,95 @@ public class LoginActivity extends AppCompatActivity {
     public void doLogin(View v){
         userName = userIdEditText.getText().toString();
         userPass = userPassEditText.getText().toString();
-        String usernameString = "delboy1";
         if(loginType.equals("D")) {
-            if (userName.equals(usernameString) && userPass.equals("d")) {
+
+
+
+
+        }
+
+    });
+
+            if (
+                    //check if email field is empty
+                if (TextUtils.isEmpty(email)){
+        mEmail.setError("Email is required.");
+        return;
+    }
+
+    //check if password field is empty
+                if (TextUtils.isEmpty(password)){
+        mPassword.setError("Please enter your password");
+        return;
+    }
+
+                if (password.length() (6){
+        mPassword.setError("Password must have more than six characters");
+        return;
+    }) {
                 Intent intent = new Intent(this, DboyActivity.class);
-                intent.putExtra(Constants.CURRENT_DELBOY, usernameString);
                 startActivity(intent);
             } else {
                 Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
             }
         } else {
-            String userId = "kalyan123";
-            if (userName.equals(userId) && userPass.equals("k")) {
-                Intent intent = new Intent(this, ShowUserOrdersActivity.class);
-                intent.putExtra(Constants.CURRENT_USER, userId);
-                startActivity(intent);
+
+            if (
+                    //check if email field is empty
+        if (TextUtils.isEmpty(email)){
+        mEmail.setError("Email is required.");
+        return;
+        }
+
+        //check if password field is empty
+        if (TextUtils.isEmpty(password)){
+        mPassword.setError("Please enter your password");
+        return;
+        }
+
+        if (password.length() < 6){
+        mPassword.setError("Password must have more than six characters");
+        return;
+        }) {
+                Intent intent = new Intent(this, ShowUserOrdersActivity.class
+                startActivity(intent); //correct
             } else {
                 Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
             }
         }
 
     }
+                    //authenticating user
+                    fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+@Override
+public void onComplete(@NonNull Task<AuthResult> task) {
+        //checks whether task was successful
+        if (task.isSuccessful()){
+        Toast.makeText(LogIn.this, "Login Successful", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        else {
+        Toast.makeText(LogIn.this, "Error:" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+        Toast.LENGTH_SHORT).show();
+        }
+        }
+        });
+
+        }
+
+        });
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(), SignUp.class));
+        }
+        });
+
+
+        }
+        }
+        }
+        });
+
 }
--- app/src/main/java/com/challengers/trackmyorder/SignUp.java
+++ app/src/main/java/com/challengers/trackmyorder/SignUp.java
@@ -6,6 +6,9 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
@@ -36,43 +39,72 @@ public class SignUp extends AppCompatActivity {
         progressDialog.setMessage("Registering User Statement..");
         progressDialog.show();
 
+        AdapterView.OnItemSelectedListener{
+            String[] user = { "Customer", "Driver"};
 
-        //Firebase authentication (account save)
-        Object firebaseAuth;
-        firebaseAuth = FirebaseAuth.getInstance();
-        Task<AuthResult> users = ((FirebaseAuth) firebaseAuth).createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            //user already logged in
-                            //start the activity
-                            finish();
-                            onAuthSuccess(task.getResult().getUser());
-                            // startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else {
-                            Object user = null;
-                            Object userId = null;
-                            Firebase mDatabase = null;
-                            mDatabase.child("users").child((String) userId).setValue(user);
-                        }
-                    }
+            @Override
+            protected void onCreate(Bundle Bundle savedInstanceState;
+            savedInstanceState); {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_sign_up);
+                //Getting the instance of Spinner and applying OnItemSelectedListener on it
+                Spinner spin = (Spinner) findViewById(R.id.spinner);
+                spin.setOnItemSelectedListener(this);
 
-                    private void onAuthSuccess(FirebaseUser user) {
+                //Creating the ArrayAdapter instance having the country list
+                ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,user);
+                aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                //Setting the ArrayAdapter data on the Spinner
+                spin.setAdapter(aa);
+
+            }
+
+            //Performing action onItemSelected and onNothing selected
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+                Toast.makeText(getApplicationContext(),user[position] , Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+        }
+    }
+    //Firebase authentication (account save)
+    Object firebaseAuth;
+    firebaseAuth = FirebaseAuth.getInstance();
+    Task<AuthResult> users = ((FirebaseAuth) firebaseAuth).createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //user already logged in
+                        //start the activity
+                        finish();
+                        onAuthSuccess(task.getResult().getUser());
+                        // startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    } else {
+                        Object user = null;
+                        Object userId = null;
+                        Firebase mDatabase = null;
+                        mDatabase.child("users").child((String) userId).setValue(user);
                     }
+                }
+
+                private void onAuthSuccess(FirebaseUser user) {
+                }
     if(toggleBut.isChecked())
 
-                    {
-                        int toggle_val = 2;
-                        //Toast.makeText(SignupActivity.this, "You're Driver", Toast.LENGTH_SHORT).show();
-                    }
+                {
+                    int toggle_val = 2;
+                    //Toast.makeText(SignupActivity.this, "You're Driver", Toast.LENGTH_SHORT).show();
+                }
     else
 
-                    {
-                        int toggle_val = 1;
-                        //Toast.makeText(SignupActivity.this, "You're Customer", Toast.LENGTH_SHORT).show();
-                    }
+                {
+                    int toggle_val = 1;
+                    //Toast.makeText(SignupActivity.this, "You're Customer", Toast.LENGTH_SHORT).show();
+                }
 
-                });
-    }
+            });
 }
